e5480daa0e9015784b74d4151547a5906b76ae27
KISLY74 <kisly.nikita@mail.ru>
2023-04-01 15:16:31 +0300

    feat: add main content of main page

--- src/pages/Main-page/Main.jsx
+++ src/pages/Main-page/Main.jsx
@@ -1,8 +1,10 @@
-import Header from "../../components/Header/Header"
+import Header from "components/Header/Header"
+import MainMovies from "components/Main-MainMovies/MainMovies"
 
 const Main = () => {
   return <div className="main-container">
     <Header />
+    <MainMovies/>
   </div>
 }
 

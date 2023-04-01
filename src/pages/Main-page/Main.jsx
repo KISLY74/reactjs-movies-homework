647edc3ce610b59374d6f23750b4717b1c20ac17
KISLY74 <kisly.nikita@mail.ru>
2023-04-01 15:44:54 +0300

    feat: add MainMovies component to main page

--- src/pages/Main-page/Main.jsx
+++ src/pages/Main-page/Main.jsx
@@ -1,10 +1,11 @@
+import './Main.scss'
 import Header from "components/Header/Header"
 import MainMovies from "components/Main-MainMovies/MainMovies"
 
 const Main = () => {
   return <div className="main-container">
     <Header />
-    <MainMovies/>
+    <MainMovies />
   </div>
 }
 

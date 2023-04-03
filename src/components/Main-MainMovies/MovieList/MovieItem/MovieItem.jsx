0138ab05e9fa5e388f11641285989a390ecb6dbd
KISLY74 <kisly.nikita@mail.ru>
2023-04-03 23:26:16 +0300

    feat: add prop of hide/show name and genres of item

--- src/components/Main-MainMovies/MovieList/MovieItem/MovieItem.jsx
+++ src/components/Main-MainMovies/MovieList/MovieItem/MovieItem.jsx
@@ -1,13 +1,15 @@
 import image from "assets/img.webp"
 
-const MovieItem = ({ movie, genres }) => {
+const MovieItem = ({ movie, genres, isHideDesc }) => {
   return <div className="item">
     <p className={`item__vote-average ${movie.vote_average >= 7 ? 'from-seven' : 'up-to-seven'}`}>
       {movie.vote_average}
     </p>
     <img className="item__img" src={image} alt="movie_image" />
-    <h2 className="item__title">{movie.title}</h2>
-    <div className="genres">{genres.map(genre => <p>{genre}</p>)}</div>
+    {!isHideDesc ? <>
+      <h2 className="item__title">{movie.title}</h2>
+      <div className="genres">{genres.map(genre => <p>{genre}</p>)}</div>
+    </> : false}
   </div>
 }
 

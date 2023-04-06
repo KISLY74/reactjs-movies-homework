a7e7df9ba2dd5038d305f155c12343ee3ff5fe9c
KISLY74 <kisly.nikita@mail.ru>
2023-04-06 12:20:44 +0300

    feat: create action creators to fetch movies by category and get all genres

---
+++ src/store/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+const BASE_URL = 'https://api.themoviedb.org/3/'
+const API_KEY = '5216e3c10482369f5b2914500256cc13'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
+
+const config = { params: { api_key: API_KEY } }
+
+export const getPopularMovies = createAsyncThunk(`moviesPopular`, () => {
+  return axios.get(BASE_URL + `movie/popular`, config).then(res => res.data.results)
+})
+
+export const getTopRatedMovies = createAsyncThunk(`moviesTopRated`, () => {
+  return axios.get(BASE_URL + 'movie/top_rated', config).then(res => res.data.results)
+})
+
+export const getUpcomingMovies = createAsyncThunk(`moviesUpcoming`, () => {
+  return axios.get(BASE_URL + 'movie/upcoming', config).then(res => res.data.results)
+})
+
+export const getAllGenres = createAsyncThunk(`allGenres`, () => {
+  return axios.get(BASE_URL + 'genre/movie/list', config).then(res => res.data.genres)
+})

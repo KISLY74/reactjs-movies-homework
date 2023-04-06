9c2f00284099eb158794a443a6ec8350dc23b10b
KISLY74 <kisly.nikita@mail.ru>
2023-04-06 18:42:43 +0300

    feat: add pagination component to main page

--- src/components/Main-MainMovies/MainMovies.jsx
+++ src/components/Main-MainMovies/MainMovies.jsx
@@ -1,10 +1,12 @@
 import CategoriesTabs from "./CategoriesTabs/CategoriesTabs"
 import MovieList from "./MovieList/MovieList"
+import Pagination from "./Pagination/Pagination"
 
 const MainMovies = () => {
   return <main className="main">
     <CategoriesTabs />
     <MovieList />
+    <Pagination />
   </main>
 }
 

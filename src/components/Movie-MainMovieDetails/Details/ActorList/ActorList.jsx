3f43e53289cff50b9d18da38a6c6c2cc8eb21c66
KISLY74 <kisly.nikita@mail.ru>
2023-04-08 00:17:21 +0300

    feat: set persons state and get person by id in url

--- src/components/Movie-MainMovieDetails/Details/ActorList/ActorList.jsx
+++ src/components/Movie-MainMovieDetails/Details/ActorList/ActorList.jsx
@@ -1,8 +1,20 @@
+import { useParams } from "react-router-dom"
 import ActorItem from "./ActorItem/ActorItem"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getCastByMovieId } from "store/api"
 
 const ActorList = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  const { persons } = useSelector(state => state.actor)
+
+  useEffect(() => {
+    dispatch(getCastByMovieId(id))
+  }, [dispatch, id])
+
   return <div className="actor-list">
-    {new Array(6).fill(<ActorItem />, 0)}
+    {persons.map(person => <ActorItem person={person} />)}
   </div>
 }
 

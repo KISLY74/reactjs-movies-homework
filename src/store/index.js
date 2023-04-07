252d9a8175a9fce1df934fb1056dbb3351dff0ee
KISLY74 <kisly.nikita@mail.ru>
2023-04-08 00:13:21 +0300

    feat: add reducers of actor and details

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { CategoriesTabsReducer } from "modules/CategoriesTabs/CategoriesTabs.slice";
 import { MovieReducer } from "modules/MovieList/MovieList.slice.js"
 import { PaginationReducer } from "modules/Pagination/Pagination.slice";
+import { MainMovieDetailsReducer } from "components/Movie-MainMovieDetails/MainMovieDetails.slice";
+import { ActorListReducer } from "components/Movie-MainMovieDetails/Details/ActorList/ActorList.slice";
 
 export const store = configureStore({
   reducer: {
     movie: MovieReducer,
     categories: CategoriesTabsReducer,
-    pagination: PaginationReducer
+    pagination: PaginationReducer,
+    details: MainMovieDetailsReducer,
+    actor: ActorListReducer
   }
 })

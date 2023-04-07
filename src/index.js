eab12c91625e50fc127ff24adb100c52f3b140ed
KISLY74 <kisly.nikita@mail.ru>
2023-04-08 00:10:19 +0300

    feat: add router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 import { SkeletonTheme } from 'react-loading-skeleton';
+import AppRouter from 'components/AppRouter/AppRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
-        <App />
+        <AppRouter />
       </SkeletonTheme>
     </Provider>
   </React.StrictMode >

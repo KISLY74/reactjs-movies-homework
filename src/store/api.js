2cb7047d2eb2d90bb8114535d311194204dc3257
KISLY74 <kisly.nikita@mail.ru>
2023-04-07 12:18:05 +0300

    feat:add parameter of page to request api

--- src/store/api.js
+++ src/store/api.js
@@ -6,15 +6,18 @@ export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
 
 const config = { params: { api_key: API_KEY } }
 
-export const getPopularMovies = createAsyncThunk(`moviesPopular`, () => {
+export const getPopularMovies = createAsyncThunk(`moviesPopular`, async (page) => {
+  config.params.page = page
   return axios.get(BASE_URL + `movie/popular`, config).then(res => res.data.results)
 })
 
-export const getTopRatedMovies = createAsyncThunk(`moviesTopRated`, () => {
+export const getTopRatedMovies = createAsyncThunk(`moviesTopRated`, async (page) => {
+  config.params.page = page
   return axios.get(BASE_URL + 'movie/top_rated', config).then(res => res.data.results)
 })
 
-export const getUpcomingMovies = createAsyncThunk(`moviesUpcoming`, () => {
+export const getUpcomingMovies = createAsyncThunk(`moviesUpcoming`, async (page) => {
+  config.params.page = page
   return axios.get(BASE_URL + 'movie/upcoming', config).then(res => res.data.results)
 })
 

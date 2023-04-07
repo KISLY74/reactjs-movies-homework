91eef99c82815a65bbb177f9dc13ec391d760a25
KISLY74 <kisly.nikita@mail.ru>
2023-04-07 12:15:27 +0300

    feat: add skeleton theme

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from 'store';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
+        <App />
+      </SkeletonTheme>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode >
 );

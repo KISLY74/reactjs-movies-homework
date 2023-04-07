bcc810101e4bac61c872b0bc4b914902eba0fe0a
KISLY74 <kisly.nikita@mail.ru>
2023-04-07 12:18:54 +0300

    feat: add implementation set page to store

--- src/modules/Pagination/Pagination.jsx
+++ src/modules/Pagination/Pagination.jsx
@@ -1,15 +1,27 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
+import { useDispatch } from "react-redux"
+import { setPage } from "./Pagination.slice"
 
 const Pagination = () => {
+  const dispatch = useDispatch()
   const [pages] = useState([1, 2, 3, 4, 5])
   const pagesRef = useRef()
 
-  useEffect(() => {
-    pagesRef.current.children[0].classList.add('pagination__page_active')
-  }, [])
+  function handleClickPage(elem) {
+    let pages = pagesRef.current.children
+
+    for (let i = 0; i < pages.length; i++)
+      pages[i].classList.remove('pagination__page_active')
+
+    elem.classList.add('pagination__page_active')
+    dispatch(setPage(elem.textContent))
+  }
 
   return <div className="pagination" ref={pagesRef}>
-    {pages.map(page => <p key={page} className="pagination__page">{page}</p>)}
+    {pages.map((page, ind) =>
+      <p key={page}
+        className={`pagination__page ${ind === 0 ? 'pagination__page_active' : ''}`}
+        onClick={(e) => handleClickPage(e.target)}>{page}</p>)}
   </div>
 }
 

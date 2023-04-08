8b257c5f62bfac1d08b77f9d6a5a2bd71c42c9df
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 00:57:08 +0300

    feat: add change state of recomendations and images

--- src/components/Movie-MainMovieDetails/MainMovieDetails.slice.js
+++ src/components/Movie-MainMovieDetails/MainMovieDetails.slice.js
@@ -1,8 +1,11 @@
-import { getMovieById } from "store/api";
+import { getImagesByMovie, getMovieById, getRecommendations } from "store/api";
 const { createSlice } = require("@reduxjs/toolkit");
 
 const initialState = {
-  movie: {}
+  movie: {},
+  images: [],
+  recommendations: [],
+  loading: false
 }
 
 const MainMovieDetailsSlice = createSlice({
@@ -11,6 +14,16 @@ const MainMovieDetailsSlice = createSlice({
   extraReducers: builder => {
     builder.addCase(getMovieById.fulfilled, (state, action) => {
       state.movie = action.payload
+    })
+    builder.addCase(getImagesByMovie.fulfilled, (state, action) => {
+      state.images = action.payload
+    })
+    builder.addCase(getRecommendations.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(getRecommendations.fulfilled, (state, action) => {
+      state.loading = false
+      state.recommendations = action.payload
     })
   }
 })

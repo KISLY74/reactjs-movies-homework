1ba858b57a8cb12c81890c20413669538fda5b2e
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 00:48:29 +0300

    feat: navigate by actor and view actor info

--- src/components/Movie-MainMovieDetails/Details/ActorList/ActorItem/ActorItem.jsx
+++ src/components/Movie-MainMovieDetails/Details/ActorList/ActorItem/ActorItem.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import { IMAGE_URL } from "store/api"
 import image from "assets/img.webp"
 
-const ActorItem = () => {
-  return <div className="actor-item">
-    <img src={image} alt="actor_image" />
-    <p className="actor-item__name">Actor Name</p>
-    <p className="actor-item__character">Played Character</p>
+const ActorItem = ({ person }) => {
+  const history = useNavigate()
+
+  return <div className="actor-item" onClick={() => history(`/person/${person.id}`)}>
+    <img src={!person.profile_path ? image : IMAGE_URL + person.profile_path} alt="actor_image" />
+    <p className="actor-item__name">{person.name}</p>
+    <p className="actor-item__character">{person.character}</p>
   </div>
 }
 

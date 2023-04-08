eadbeadde2fc5505522a9c0cd5e44e1fb4328ec3
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 00:46:46 +0300

    feat: set state of person info, images and cast movies and view them

--- src/components/Actor-MainActorProfile/MainActorProfile.jsx
+++ src/components/Actor-MainActorProfile/MainActorProfile.jsx
@@ -1,39 +1,48 @@
-import image from "assets/img.webp"
 import MovieList from "modules/MovieList/MovieList"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { IMAGE_URL, getCastMovieByPerson, getImagesByPerson, getPersonById } from "store/api"
 
 const MainActorProfile = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  const actorProfile = useSelector(state => state.actorProfile)
+
+  useEffect(() => {
+    dispatch(getPersonById(id))
+    dispatch(getImagesByPerson(id))
+    dispatch(getCastMovieByPerson(id))
+  }, [dispatch, id])
+
   return <main className="main">
     <section className="actor-info">
-      <img src={image} alt="" />
+      <img src={IMAGE_URL + actorProfile.person.profile_path} alt="" />
       <div className="info">
-        <h3 className="info__name">Actor Name</h3>
+        <h3 className="info__name">{actorProfile.person.name}</h3>
         <section className="section">
           <h5 className="section__title">Birthday:</h5>
-          <p className="section__value">1959-04-15</p>
+          <p className="section__value">{actorProfile.person.birthday}</p>
         </section>
         <section className="section">
           <h5 className="section__title">Place of birth:</h5>
-          <p className="section__value">Paddington, London, England</p>
+          <p className="section__value">{actorProfile.person.place_of_birth}</p>
         </section>
         <section className="section">
           <h5 className="section__title">Biography:</h5>
-          <p className="section__value">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          <p className="section__value">{actorProfile.person.biography}</p>
         </section>
         <section className="section-photos">
           <h4>Photos</h4>
           <div className="photos">
-            {new Array(4).fill(<img src={image} alt="" />)}
+            {actorProfile.images.map(im => <img src={IMAGE_URL + im.file_path} alt="" />)}
           </div>
         </section>
       </div>
     </section>
     <section className="actor-films">
       <h2 className="actor-films__title">KNOWN BY</h2>
-      <MovieList movies={new Array(10).fill({
-        vote_average: 7.4,
-        title: 'Spider man',
-        genres: ["Genre 1", "Genre 2", "Genre 3"]
-      }, 0)} />
+      <MovieList actorProfile={actorProfile} />
     </section>
   </main>
 }

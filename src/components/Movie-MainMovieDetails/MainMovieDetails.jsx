742354edd061b4eab8b4b2c0dab9e342482003b7
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 00:57:54 +0300

    feat: use id by url and change state of recommendations and images movie

--- src/components/Movie-MainMovieDetails/MainMovieDetails.jsx
+++ src/components/Movie-MainMovieDetails/MainMovieDetails.jsx
@@ -1,12 +1,25 @@
 import MovieList from "modules/MovieList/MovieList"
 import Details from "./Details/Details"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { getImagesByMovie, getRecommendations } from "store/api"
 
 const MainMovieDetails = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+  const details = useSelector(state => state.details)
+
+  useEffect(() => {
+    dispatch(getImagesByMovie(id))
+    dispatch(getRecommendations(id))
+  }, [dispatch, id])
+
   return <main className="main">
     <Details />
     <section className="recomendations">
       <h2 className="recomendations__title">RECOMENDATIONS</h2>
-      <MovieList />
+      <MovieList details={details} />
     </section>
   </main>
 }

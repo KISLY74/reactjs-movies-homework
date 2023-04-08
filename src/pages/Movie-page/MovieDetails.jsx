c3d5de21997eb51a296cf6cc61e7faeb059330a7
KISLY74 <kisly.nikita@mail.ru>
2023-04-08 10:57:53 +0300

    feat: set movie id to state from url

--- src/pages/Movie-page/MovieDetails.jsx
+++ src/pages/Movie-page/MovieDetails.jsx
@@ -1,8 +1,19 @@
 import "./MovieDetails.scss"
 import Header from "components/Header/Header"
 import MainMovieDetails from "components/Movie-MainMovieDetails/MainMovieDetails"
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getMovieById } from "store/api"
 
 const MovieDetails = () => {
+  const { id } = useParams()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getMovieById(id))
+  }, [dispatch, id])
+
   return <>
     <Header title={"Movie Details"} />
     <MainMovieDetails />

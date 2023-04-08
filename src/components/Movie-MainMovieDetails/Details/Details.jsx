55041d807bd8ac41da05fd1216a61db08001a287
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 00:58:42 +0300

    feat: get data from store and view them

--- src/components/Movie-MainMovieDetails/Details/Details.jsx
+++ src/components/Movie-MainMovieDetails/Details/Details.jsx
@@ -1,42 +1,40 @@
 import MovieItem from "components/Main-MainMovies/MovieItem/MovieItem"
+import { useSelector } from "react-redux"
 import ActorList from "./ActorList/ActorList"
-import image from "assets/img.webp"
+import { IMAGE_URL } from "store/api"
 
 const Details = () => {
+  const { movie, images } = useSelector(state => state.details)
+
   return <section className="details">
     <section className="info">
       <MovieItem
-        movie={{
-          vote_average: 8.335,
-          title: 'Spider man'
-        }}
+        movie={movie}
         genres={["Genre 1", "Genre 2", "Genre 3"]}
         isHideDesc={true} />
       <div className="info-container">
         <div className="title">
           <h4>Title:</h4>
-          <p className="title__value">Movie Title</p>
+          <p className="title__value">{movie.title}</p>
         </div>
         <div className="overview">
           <h4>Overview:</h4>
-          <p className="overview__value">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          <p className="overview__value">{movie.overview}</p>
         </div>
         <div className="date">
           <h4>Release date:</h4>
-          <p className="date__value">2021-05-26</p>
+          <p className="date__value">{movie.release_date}</p>
         </div>
         <div className="revenue">
           <h4>Revenue:</h4>
-          <p className="revenue__value">$ 42 600 000</p>
+          <p className="revenue__value">$ {movie.revenue}</p>
         </div>
         <div className="duration">
           <h4>Duration:</h4>
-          <p className="duration__value">2:14</p>
+          <p className="duration__value">{movie.runtime} min</p>
         </div>
         <div className="genres">
-          <p>Genre 1</p>
-          <p>Genre 2</p>
-          <p>Genre 3</p>
+          {movie?.genres?.map(genre => <p>{genre.name}</p>)}
         </div>
         <section className="cast">
           <div className="cast-up-container">
@@ -48,7 +46,7 @@ const Details = () => {
         <section className="images">
           <h4>Images:</h4>
           <div className="images-content">
-            {new Array(8).fill(<img src={image} alt="" />)}
+            {images.map(im => <img src={IMAGE_URL + im.file_path} alt="" />)}
           </div>
         </section>
       </div>

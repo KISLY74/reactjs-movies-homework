e87ff0b514104947a711e46f523254016631d276
KISLY74 <kisly.nikita@mail.ru>
2023-04-10 00:03:26 +0300

    feat: add error boundary of app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { Provider } from 'react-redux';
 import { store } from 'store';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import AppRouter from 'components/AppRouter/AppRouter';
+import { ErrorBoundary } from "react-error-boundary";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
-        <AppRouter />
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <AppRouter />
+        </ErrorBoundary>
       </SkeletonTheme>
     </Provider>
   </React.StrictMode >

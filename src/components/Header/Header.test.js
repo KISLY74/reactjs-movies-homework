b4acdab3fbc9183d1d1c2ef9ac89aea841f8f809
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 23:55:28 +0300

    test: remove check of movie and actor pages expect return search input

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -10,12 +10,10 @@ describe("Header", () => {
   })
   it("render Header component on the Movie-page", () => {
     render(<Header title="Movie details" />);
-    expect(screen.getByPlaceholderText('Movies, person, movie theaters')).toBeInTheDocument();
     expect(screen.getByText(/Movie details/i)).toBeInTheDocument();
   })
   it("render Header component on the Actor-page", () => {
     render(<Header title="Actor profile" />);
-    expect(screen.getByPlaceholderText('Movies, person, movie theaters')).toBeInTheDocument();
     expect(screen.getByText(/Actor profile/i)).toBeInTheDocument();
   })
 })

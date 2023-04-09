bc463a140a4805883d7e5a15d1caf9b9a9126553
KISLY74 <kisly.nikita@mail.ru>
2023-04-09 21:28:44 +0300

    feat: on keydown set movies

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getMoviesBySearchQuery } from "store/api"
+import { changeValue } from "./SearchInput.slice"
 
 const SearchInput = () => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState('')
+  const dispatch = useDispatch()
+  const page = useSelector(state => state.pagination.page)
+
+  function send(key) {
+    if (key === 'Enter') {
+      dispatch(changeValue(value))
+      dispatch(getMoviesBySearchQuery({ value: value, page: page }))
+    }
+  }
+
+  useEffect(() => {
+    value.trim() !== '' && dispatch(getMoviesBySearchQuery({ value: value, page: page }))
+  }, [dispatch, page, value])
 
   return <input
     className="search-language__search"
     type="text"
     value={value}
     onChange={(e) => setValue(e.target.value)}
+    onKeyDown={(e) => send(e.key)}
     placeholder="Movies, person, movie theaters" />
 }
 
